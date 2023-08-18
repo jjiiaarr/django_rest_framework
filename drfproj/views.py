053157bdb79475c95813fb012a80a30d7f5643ae
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-18 15:34:52 +0800

    add serializer and authentication on views

--- drfproj/views.py
+++ drfproj/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# from drfproj.drfapp.serializers import StudentSerializer
-
-from .serializers import StudentsSerializer
-from .models import Student
+from drfapp.serializers import StudentSerializer
+from drfapp.models import Student
 
 
 class TesView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'years_active': 10
-        }
-        return Response(data)
+        qs = Student.objects.all()
+        student1 = qs.first()
+        serializer = StudentSerializer(student1)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors)
